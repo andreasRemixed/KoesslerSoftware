d220434022ef5b5d8e6213086903d5ff3a5791c4
andreasRemixed <aigner.andreas@anzmark-aigner.local>
2020-08-21 13:41:51 +0200

    finished tab functionality, add scroll behavior

--- js/tabs.js
+++ js/tabs.js
@@ -1,21 +1,22 @@
 const tabs = document.querySelectorAll(".tabs__tab");
-const tabsHeadline = document.querySelectorAll(".tabs__headline");
 const content = document.querySelectorAll(".tabs__text");
-console.log(content);
+const tabsArr = Array.prototype.slice.call(tabs);
 
 tabs.forEach(tab => {
-    tab.addEventListener("click", e => {
-    resetStyles();
-    tab.classList.add("activeTab");
-    tab.children[1].classList.add("activeHeadline");
-    });
-  })
+      tab.addEventListener("click", e => {
+            resetStyles();
+            tab.classList.add("tabs__tab--active");
+            tab.children[1].classList.add("tabs__headline--active");
+            content[tabsArr.indexOf(tab)].classList.add("tabs__text--active");
+      });
+})
 
-  function resetStyles() {
-        for (i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove("activeTab")
-        tabs[i].children[1].classList.remove("activeHeadline");
-        }
-  }
-
-  
+function resetStyles() {
+      for (i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove("tabs__tab--active")
+            tabs[i].children[1].classList.remove("tabs__headline--active");
+            content.forEach(text => {
+                  text.classList.remove("tabs__text--active");
+            })
+      }
+}
